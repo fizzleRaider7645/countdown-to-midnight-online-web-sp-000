a69cfca26e79e27c137d651bdb09e90f1bef3884
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-02 12:04:15 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(seconds)
   return "HAPPY NEW YEAR!" if seconds == 12
   count = seconds
   while count >= 0
-  puts "#{seconds} SECOND(S)!"
+  puts "#{count} SECOND(S)!"
     count -= 1
   end
 end
